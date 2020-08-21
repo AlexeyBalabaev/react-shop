eb0f63a874853a2b595cd3e527e9f250f31d09d4
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-08-22 01:40:12 +0300

    axios/PropTypes etc.

--- src/components/PizzaElem.jsx
+++ src/components/PizzaElem.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-function PizzaElem({ name, imageUrl, price }) {
-  const types = ['тонкое', 'традиционное'];
-  const [activeType, setActiveType] = React.useState(1);
+function PizzaElem({ name, imageUrl, price, types, sizes }) {
+  const availableTypes = ['тонкое', 'традиционное'];
+  const availableSizes = [26, 30, 40];
+
+  const [activeType, setActiveType] = React.useState(types[0]);
+  const [activeSize, setActiveSize] = React.useState(sizes[0]);
 
   const onSelectType = (index) => {
     setActiveType(index);
+  };
+
+  const onSelectSize = (index) => {
+    setActiveSize(index);
   };
 
   return (
@@ -14,18 +23,30 @@ function PizzaElem({ name, imageUrl, price }) {
       <h4 className="pizza-block__title">{name}</h4>
       <div className="pizza-block__selector">
         <ul>
-          {types.map((type, index) => (
+          {availableTypes.map((type, index) => (
             <li 
+              key={type}
               onClick={() => onSelectType(index)} 
-              className={activeType === index ? 'active' : ''}>
+              className={classNames({
+                active: activeType === index,
+                disabled: !types.includes(index),
+              })}>
               {type}
             </li>
           ))}
         </ul>
         <ul>
-          <li className="active">26 см.</li>
-          <li>30 см.</li>
-          <li>40 см.</li>
+          {availableSizes.map((size, index) => (
+            <li 
+              key={size}
+              onClick={() => onSelectSize(index)} 
+              className={classNames({
+                active: activeSize === index,
+                disabled: !sizes.includes(size),
+              })}>
+              {size} см.
+            </li>
+          ))}
         </ul>
       </div>
       <div className="pizza-block__bottom">
@@ -50,5 +71,13 @@ function PizzaElem({ name, imageUrl, price }) {
     </div>
   )
 }
+
+PizzaElem.propTypes = {
+  name: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  types: PropTypes.arrayOf(PropTypes.number).isRequired,
+  sizes: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
 
 export default PizzaElem;
