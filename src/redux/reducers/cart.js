eed420cd6a90b6413fb96340d5417ce4a1982e28
Alexey Balabaev <jhon.snow.verysnow@yandex.ru>
2020-09-09 17:51:57 +0300

    working to cart

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -4,19 +4,27 @@ const initialState = {
   totalCount: 0,
 };
 
+const getTotalPrice = arr => arr.reduce((sum, obj) => obj.price + sum, 0);
+
 const cart = (state = initialState, action) => {
   switch (action.type) {
 
     case 'ADD_PIZZA_CART':{
+      const currentIdPizzaItems = !state.items[action.payload.id] 
+        ? [action.payload] 
+        : [...state.items[action.payload.id].items, action.payload];
+
       const newItems = {
         ...state.items,
-          [action.payload.id]: !state.items[action.payload.id] 
-            ? [action.payload] 
-            : [...state.items[action.payload.id], action.payload],
+          [action.payload.id]: {
+            items: currentIdPizzaItems,
+            totalPrice: getTotalPrice(currentIdPizzaItems),
+          },
       }
 
-      const allPizzas = [].concat.apply([], Object.values(newItems));
-      const totalPrice = allPizzas.reduce((sum, obj) => obj.price + sum, 0);
+      const items = Object.values(newItems).map(obj => obj.items);
+      const allPizzas = [].concat.apply([], items);
+      const totalPrice = getTotalPrice(allPizzas);
 
       return {
         ...state,
@@ -25,6 +33,13 @@ const cart = (state = initialState, action) => {
         totalPrice: totalPrice,
       };
     }
+
+    case 'CLEAR_CART':
+      return {
+        items: {},
+        totalPrice: 0,
+        totalCount: 0,
+      };
 
     default:
       return state;
